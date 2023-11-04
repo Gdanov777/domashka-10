4ad5adcef1ebf0ffc92f7ab26b423e1d515dcb58
Gdanov777 <128631304+Gdanov777@users.noreply.github.com>
2023-11-04 15:35:25 +0200

    sum

--- src/index.js
+++ src/index.js
@@ -1,45 +1,73 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { Report } from 'notiflix/build/notiflix-report-aio';
+import { fetchBreeds, fetchCatByBreed } from './js/cat-api.js';
+import Notiflix from 'notiflix';
 import SlimSelect from 'slim-select';
-import { fetchBreeds, fetchCatByBreed } from './js/api';
-import { createMarkup, createMarkupCat } from './js/markup';
-import refs from './js/refs';
 
-refs.selectEl.addEventListener('change', onValueId);
-fetchBreeds()
-  .then(arr => {
-    load();
-    refs.selectEl.innerHTML =
-      '<option value= "" selected disabled>Choose your cat</option> ';
-    return (refs.selectEl.innerHTML += createMarkup(arr.data));
-  })
-  .then(() => slim())
-  .catch(fetchError);
 
-function onValueId(e) {
-  const id = e.target.value;
-  fetchCatByBreed(id)
-    .then(obj => {
-      load();
+const catInfoEl = document.querySelector('.cat-info');
+const selectEl = document.querySelector('.breed-select');
 
-      return (refs.catInfoEl.innerHTML = createMarkupCat(obj.data));
+selectEl.addEventListener('change', onBreedSelect);
+
+function onBreedSelect(event) {
+  Notiflix.Loading.standard('Loading data, please wait...');
+  catInfoEl.innerHTML = '';
+
+  const breedPr = fetchCatByBreed(event.target.value);
+
+  breedPr
+    .then(breed => {
+      createMarkupCard(breed[0]);
+      Notiflix.Loading.remove();
     })
-    .then(() => success())
-    .catch(fetchError);
-}
-function fetchError() {
-  Report.failure(refs.error.textContent, '');
-}
-function success() {
-  Notify.success('Search was successful!)', '');
-}
-function load() {
-  refs.selectEl.hidden = false;
-  refs.loaderEl.classList.remove('loader');
+    .catch(error => {
+      Notiflix.Loading.remove();
+      Notiflix.Notify.failure(
+        'Oops! Something went wrong! Try reloading the page!'
+      );
+    });
 }
 
-function slim() {
+function createMarkupSelect(breeds) {
+  const markupSelect = breeds
+    .map(breed => {
+      return `<option value="${breed.id}">${breed.name}</option>`;
+    })
+    .join();
+
+  selectEl.innerHTML = markupSelect;
+
+  selectEl.style.visibility = 'inherit';
   new SlimSelect({
-    select: refs.selectEl,
+    select: '.breed-select',
   });
 }
+
+function createMarkupCard(breed) {
+  const { url, breeds } = breed;
+  const { name, description, temperament } = breeds[0];
+
+  const markupSelect = `
+  <h1>${name}</h1>
+  <p>${description}</p>
+  <p><strong>Temperament: </strong>${temperament}</p>
+  <img src=${url}>`;
+
+  catInfoEl.innerHTML = markupSelect;
+}
+
+Notiflix.Loading.standard('Loading data, please wait...');
+
+const breedsPr = fetchBreeds();
+
+breedsPr
+  .then(breeds => {
+    createMarkupSelect(breeds);
+
+    Notiflix.Loading.remove();
+  })
+  .catch(() => {
+    Notiflix.Loading.remove();
+    Notiflix.Notify.failure(
+      'Oops! Something went wrong! Try reloading the page!'
+    );
+  });
